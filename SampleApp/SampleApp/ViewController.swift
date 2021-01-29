368c224fa2ec2adaf43f349ce49f935676b4499a
tokorom <tokorom@gmail.com>
2021-01-29 14:57:31 +0900

    Update SampleApp

--- SampleApp/SampleApp/ViewController.swift
+++ SampleApp/SampleApp/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import SwiftSuperellipse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let superellipseView = SuperellipseView(frame: CGRect(x: 10, y: 10, width: 200, height: 150))
+        view.addSubview(superellipseView)
     }
 
 
