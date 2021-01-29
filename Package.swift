7056ba860447fb58d36de040566cc3d2c553f642
tokorom <tokorom@gmail.com>
2021-01-29 14:18:20 +0900

    Update Package.name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-superellipse",
+    name: "SwiftSuperellipse",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
