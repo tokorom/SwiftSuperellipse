d75cdb0f7331f03effe6e84620ad545ae82a3dd1
tokorom <tokorom@gmail.com>
2021-01-29 11:03:54 +0900

    Add a new project

---
+++ swift-superellipse/swift-superellipseTests/swift_superellipseTests.swift
@@ -0,0 +1,33 @@
+//
+//  swift_superellipseTests.swift
+//  swift-superellipseTests
+//
+//  Created by Yuta Tokoro on 2021/01/29.
+//
+
+import XCTest
+@testable import swift_superellipse
+
+class swift_superellipseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
