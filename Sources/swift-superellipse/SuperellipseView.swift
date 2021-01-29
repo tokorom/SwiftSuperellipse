7704ea1aabc07d1d6841896977e4c4361d61d2dc
tokorom <tokorom@gmail.com>
2021-01-29 13:52:18 +0900

    Add initializer

--- Sources/swift-superellipse/SuperellipseView.swift
+++ Sources/swift-superellipse/SuperellipseView.swift
@@ -11,6 +11,16 @@ public final class SuperellipseView: UIView {
   @IBInspectable var color: UIColor? = .systemBlue
   @IBInspectable var k: CGFloat = 0.75
 
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    backgroundColor = .clear
+  }
+
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+
   public override func draw(_ rect: CGRect) {
     color?.setFill()
     Superellipse(in: rect, k: k).bezierPath.fill()
