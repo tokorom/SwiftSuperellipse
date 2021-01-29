b306194df8741fc5577f642ac6253cf5d94d8059
tokorom <tokorom@gmail.com>
2021-01-29 14:07:59 +0900

    Add mask

--- Sources/swift-superellipse/SuperellipseView.swift
+++ Sources/swift-superellipse/SuperellipseView.swift
@@ -8,21 +8,19 @@ import UIKit
 
 @IBDesignable
 public final class SuperellipseView: UIView {
-  @IBInspectable var color: UIColor? = .systemBlue
   @IBInspectable var k: CGFloat = 0.75
 
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-
-    backgroundColor = .clear
-  }
-
-  required init?(coder: NSCoder) {
-    super.init(coder: coder)
-  }
-
   public override func draw(_ rect: CGRect) {
-    color?.setFill()
+    backgroundColor?.setFill()
     Superellipse(in: rect, k: k).bezierPath.fill()
+  }
+
+  public override func layoutSubviews() {
+    super.layoutSubviews()
+
+    let path = Superellipse(in: bounds, k: k).bezierPath
+    let mask = CAShapeLayer()
+    mask.path = path.cgPath
+    layer.mask = mask
   }
 }
